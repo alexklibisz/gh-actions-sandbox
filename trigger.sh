b25ae6e83c5608a23ac876a0ea6fe55fdc0aad62
Alex Klibisz <aklibisz@gmail.com>
2020-06-20 15:31:43 -0400

    passing benchmark param correctly

--- trigger.sh
+++ trigger.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 branch=$(git rev-parse --abbrev-ref HEAD)
 curl -H "Accept: application/vnd.github.everest-preview+json" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      --request POST \
-     --data '{"event_type": "benchmark", "client_payload": { "branch": "${branch}"}}' \
+     --data '{"event_type": "benchmark", "client_payload": { "branch": "'$branch'"}}' \
      https://api.github.com/repos/alexklibisz/gh-actions-sandbox/dispatches
