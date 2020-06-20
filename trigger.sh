3456f3ebc2729dea9e917381f508daa356809d6f
Alex Klibisz <aklibisz@gmail.com>
2020-06-20 15:24:35 -0400

    Trigger including branch name

--- trigger.sh
+++ trigger.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+branch=$(git rev-parse --abbrev-ref HEAD)
 curl -H "Accept: application/vnd.github.everest-preview+json" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      --request POST \
-     --data '{"event_type": "benchmark", "client_payload": { "text": "a title"}}' \
+     --data '{"event_type": "benchmark", "client_payload": { "branch": "${branch}"}}' \
      https://api.github.com/repos/alexklibisz/gh-actions-sandbox/dispatches
